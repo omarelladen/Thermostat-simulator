35c0f82ab4de60bd3b8b9f7f6010f5fec49c8a3f
Omar <omarzagonel@gmail.com>
2025-09-22 18:42:51 -0300

    inital commit

---
+++ defines.h
@@ -0,0 +1,44 @@
+// Serial pins
+#define PIN_D0 0
+#define PIN_D1 1
+
+#define PIN_D2 2
+#define PIN_D3 3
+
+// LCD Shield pins
+#define PIN_SHIELD_BTS A0
+#define PIN_LCD_D4 4
+#define PIN_LCD_D5 5
+#define PIN_LCD_D6 6
+#define PIN_LCD_D7 7
+#define PIN_LCD_RS 8
+#define PIN_LCD_EN 9
+#define PIN_LCD_LIGHT 10
+
+// SPI pins
+#define PIN_MOSI 11
+#define PIN_MISO 12
+#define PIN_CLK  13
+
+// Analog voltage in A0 for LCD Shield buttons
+#define SEL_THRESHOLD   800
+#define LEFT_THRESHOLD  600
+#define UP_THRESHOLD    400
+#define DOWN_THRESHOLD  200
+#define RIGHT_THRESHOLD  60
+
+// Buttons
+#define BT_NONE   0
+#define BT_SELECT 1
+#define BT_LEFT   2
+#define BT_UP     3
+#define BT_DOWN   4
+#define BT_RIGHT  5
+
+#define DEBOUNCE_TIME 50
+
+#define NUM_SCREENS 6
+
+// Temperature regulation
+#define REGULATION_INTERVAL 1000  // ms
+#define TEMP_MAX_TARGET 50 
